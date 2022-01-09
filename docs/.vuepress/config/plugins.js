44dd3568e731d572301708e62e15ffed406d8fcd
hnist <369365576@qq.com>
2022-01-09 19:09:50 +0800

    增加一些特效

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -6,6 +6,18 @@ module.exports = [
         excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     }],
 
+
+    //回到顶部插件
+    ['go-top'],
+
+    //丝带插件
+    ['ribbon',{
+            size: 90, // width of the ribbon, default: 90
+            opacity: 0.8, // opacity of the ribbon, default: 0.3
+            zIndex: -1, // z-index property of the background, default: -1
+    }],
+
+    //评论插件
     [
         'vuepress-plugin-comment',
         {
